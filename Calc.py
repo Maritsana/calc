89c56d30dd63285f9b9f39fdb620cabc2ac197a0
Maritsana <marrusy17@gmail.com>
2021-08-20 20:59:05 +0300

    Added logic and loops, restricting data entry

--- Calc.py
+++ Calc.py
@@ -1,19 +1,60 @@
-print("Здравствуйте!")
-print("Укажите Ваш пол:")
-gender = input()
 man = ['М', 'м', 'Муж', 'муж', 'Мужской', 'мужской', 'Мужчина', 'мужчина', 'M', 'm', 'Man', 'man', 'Male', 'male']
 woman = ['Ж', 'ж', 'Жен', 'жен', 'Женский', 'женский', 'Женщина', 'женщина', 'W', 'w', 'Woman', 'woman', 'Female', 'female', 'Wife', 'wife', 'Feminine', 'feminine']
-if gender in man:
-    weight = float(input("Ваш вес: "))
-    height = float(input("Ваш рост: "))
-    age = int(input("Ваш возраст: "))
-    calories = (10*weight+6.25*height-5*age+5)
-    print('Вам нужно потреблять', int(calories), 'калорий!')
-elif gender in woman:
-    weight = float(input("Ваш вес: "))
-    height = float(input("Ваш рост: "))
-    age = int(input("Ваш возраст: "))
-    calories = (10*weight+6.25*height-5*age-161)
-    print('Вам нужно потреблять', int(calories), 'калорий!')
-else:
-    print('Введите корректные данные!')
+print("Здравствуйте!")
+while True:
+    print("Укажите Ваш пол:")
+    gender = input()
+    if gender in man:
+        # def wha():
+        while True:
+            weight = float(input("Ваш вес: "))
+            if 0 < weight < 350:
+                break
+            else:
+                print('Введите корректные данные!')
+                continue
+        while True:
+            height = float(input("Ваш рост: "))
+            if 0 < height < 250:
+                break
+            else:
+                print('Введите корректные данные!')
+                continue
+        while True:
+            age = int(input("Ваш возраст: "))
+            if 0 < age < 120:
+                break
+            else:
+                print('Введите корректные данные!')
+                continue
+        calories = (10*weight+6.25*height-5*age+5)
+        print('Вам нужно потреблять', int(calories), 'калорий!')
+        # break
+    elif gender in woman:
+        while True:
+            weight = float(input("Ваш вес: "))
+            if 0 < weight < 350:
+                break
+            else:
+                print('Введите корректные данные!')
+                continue
+        while True:
+            height = float(input("Ваш рост: "))
+            if 0 < height < 250:
+                break
+            else:
+                print('Введите корректные данные!')
+                continue
+        while True:
+            age = int(input("Ваш возраст: "))
+            if 0 < age < 120:
+                break
+            else:
+                print('Введите корректные данные!')
+                continue
+        calories = (10*weight+6.25*height-5*age-161)
+        print('Вам нужно потреблять', int(calories), 'калорий!')
+        # break
+    else:
+        print('Введите корректные данные!')
+        continue
